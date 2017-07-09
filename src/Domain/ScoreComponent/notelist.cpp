cb9b1d2350363f6e5323c8d2dc045592b835f9c3
miyabishi <miyabishi1syk@gmail.com>
2017-07-09 17:45:26 +0900

    toParameters function is implemented.

--- src/Domain/ScoreComponent/notelist.cpp
+++ src/Domain/ScoreComponent/notelist.cpp
@@ -15,4 +15,15 @@ void NoteList::append(Note aNote)
 
 Parameters NoteList::toParameters() const
 {
+    Parameters parameters;
+    QJsonArray noteListArray;
+    foreach(Note note, mNoteList_)
+    {
+        noteListArray.append(note.toParameters().toJsonArray());
+    }
+
+    parameters.append(
+                Parameter("NoteList",
+                          noteListArray));
+    return parameters;
 }
